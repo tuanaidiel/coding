2bc13362cac78a402ce92bba456f9e17d8f6d1b0
Tuan Muhammad Aidiel <tuanaidiel@gmail.com>
2025-01-29 13:05:42 +0800

    Add files via upload

---
+++ Q1_triangle.py
@@ -0,0 +1,6 @@
+def triangle (n):
+
+    for i in range (1, n + 1):
+        print ("*" * i)
+
+triangle(5)
---
+++ Q1_triangle/Q1_triangle.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+class Program
+{
+    static void triangle(int n)
+    {
+        for (int i=1 ; i<=n ; i++)
+        {
+            System.Console.WriteLine(new string ('*',i));
+        }
+        
+    }
+
+    static void Main(string[] args)
+    {
+        triangle(5);
+    }
+}
