ddfcb3fba01a1b151024240555b6f0df5fae84a6
Tuan Muhammad Aidiel <tuanaidiel@gmail.com>
2025-03-04 15:04:04 +0800

    Add files via upload

---
+++ Q3/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+public class Solution
+{
+    public int StrStr(string haystack, string needle)
+    {
+        return haystack.IndexOf(needle);
+    }
+}
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Solution solution = new Solution();
+
+        System.Console.WriteLine(solution.StrStr("sadbutsad", "sad"));
+        System.Console.WriteLine(solution.StrStr("leetcode", "leeto"));
+    }
+}
