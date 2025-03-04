e3e446dbfda2faeec9f091ebbc032b866f449c82
Tuan Muhammad Aidiel <tuanaidiel@gmail.com>
2025-03-04 14:51:42 +0800

    Add files via upload

---
+++ Q1/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+public class Solution
+{
+    public int LengthOfLastWord(string s)
+    {
+        string[] words = s.Trim().Split(' ');
+        return words[words.Length - 1].Length;
+    }
+}
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Solution solution = new Solution();
+
+        // Example test cases
+        Console.WriteLine(solution.LengthOfLastWord("Hello World")); // Output: 5
+        Console.WriteLine(solution.LengthOfLastWord("   fly me   to   the moon  ")); // Output: 4
+        Console.WriteLine(solution.LengthOfLastWord("luffy is still joyboy")); // Output: 6
+    }
+}
---
+++ Q2/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+
+public class Solution
+{
+    public string LongestCommonPrefix(string[] strs)
+    {
+        if(strs == null || strs.Length == 0)
+            return "";
+
+        string prefix = strs[0];
+
+        for (int i=1 ; i<strs.Length ; i++)
+        {
+            while (strs[i].IndexOf(prefix) != 0)
+            {
+                prefix = prefix.Substring(0, prefix.Length - 1);
+                if (prefix == "")
+                    return "";
+            }
+        }
+
+        return prefix;
+    }
+}
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Solution solution = new Solution();
+
+        System.Console.WriteLine(solution.LongestCommonPrefix(new string[] { "flower", "flow", "flight" }));
+        System.Console.WriteLine(solution.LongestCommonPrefix(new string[] { "dog", "racecar", "car" }));
+    }
+}
